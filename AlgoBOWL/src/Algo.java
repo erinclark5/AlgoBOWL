f91c4ae16b8f4863440274158c86fc5458f199a2
Erin Clark <erinclark@mymail.mines.edu>
2019-02-22 15:41:38 -0700

    "found lower closest number"

--- AlgoBOWL/src/Algo.java
+++ AlgoBOWL/src/Algo.java
@@ -4,12 +4,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
-public class Algo {
+public class OutputAlgo {
 	
+
 	ArrayList<Integer> sums = new ArrayList<Integer>();
 	ArrayList<Integer> inputs = new ArrayList<Integer>();
-	
-	
+
+
 	public void readFile() {
 		File file = new File("test.txt");
 		Scanner scanner = null;
@@ -18,25 +19,25 @@ public class Algo {
 		} catch (FileNotFoundException e) {
 			System.out.println("File not found.");
 		}
-		
+
 		sums.add(1);
 		int numInputs = scanner.nextInt();
 		for(int i = 0; i < numInputs; i++) {
 			inputs.add(scanner.nextInt());	
 		}
 	}
-	
+
 	public void doubleLargest(int i) {
 		int currentValue = inputs.get(i);
 		int largestSum = sums.get(sums.size()-1);
 		while(2*largestSum <= currentValue) {
 			sums.add(2*largestSum);
 			//keep track
-			//sort arry
+			//sort array
 			largestSum = sums.get(sums.size()-1);		
 		}
 	}
-	
+
 	public void findAdd(int curInput, int maxValue) {
 		int valueNeeded = curInput - maxValue;
 		if(sums.contains(valueNeeded)) {
@@ -45,52 +46,64 @@ public class Algo {
 			//sort array
 		}
 		else {
-			 
-			modifiedBinary(0, sums.size()-1, valueNeeded);
+
+			findClosest(valueNeeded);
 		}
-	}
-	
-	public int modifiedBinary(int l, int r, int x) {
-		int mid = 0;
-		if(r > l) {
-			mid = l + (r-l)/2;
-			if(sums.get(mid) == x) {
-				return sums.get(mid);
-			}
-			if(sums.get(mid) > x) {
-				return modifiedBinary(l, mid-1, x);
-			}
-			if (sums.get(mid) < x && sums.get(mid+1) < x)
-				return modifiedBinary(mid+1, r, x);
-		}
-		else 
-			return 0;
-		return 0;
-			//return sums.get(l) + findAdd(x, sums.get(l));
 	}
 
-	
+
+	public int findClosest(int target) {
+		int n = sums.size()-1;
+		if(target <= sums.get(0)) return sums.get(0);
+		if(target >= sums.get(n-1)) return sums.get(n-1);
+
+		int i = 0, j = n, mid = 0;
+		
+		while(i < j) {
+			mid = (i+j)/2;
+			if(sums.get(mid) == target)
+				return sums.get(mid);
+			if(target < sums.get(mid)) {
+				if(mid > 0 && target > sums.get(mid-1)) {
+					return getClosest(sums.get(mid-1), sums.get(mid), target);
+				}
+				j = mid;
+			}
+			else {
+				if(mid < n-1 && target < sums.get(mid + 1)) {
+					return getClosest(sums.get(mid), sums.get(mid+1), target);
+				}
+				i = mid + 1;
+			}
+		}
+		return sums.get(mid);
+	}
+
+	public static int getClosest(int val1, int val2,  
+			int target) 
+	{ 
+			return val1;         
+	} 
+
+
 	public static void main(String[] args) {
-		Algo a = new Algo();
-		a.readFile();
+		OutputAlgo a = new OutputAlgo();
+		//a.readFile();
 		//look at the next value in the inputs array
-		for(int i = 0; i < a.inputs.size(); i++) {
+		/*for(int i = 0; i < a.inputs.size(); i++) {
 			if(2*a.sums.get(a.sums.size() - 1) <= a.inputs.get(i)) {
 				a.doubleLargest(i);
 			}
 			if(a.sums.get(a.sums.size() - 1) != a.inputs.get(i)) {
 				a.findAdd(a.inputs.get(i), a.sums.get(a.sums.size()-1));
 			}
-			
-		}
-		
-			//double the max value of the sums array until that value is larger than the current input we are at
-			//subtract max value from the current input if it is not equal to the value we are looking for
-			//modified binary search to find the value we need that when added to the max value equals the current input
-			//if no value is found, recursively binary search with next highest value that would not exceed
-		
-		
+
+		}*/
+		a.sums.add(2);
+		a.sums.add(5);
+		System.out.println(a.findClosest(4));
 	}
-	
-	
+
+
 }
+
